a9edbda083a336e642736b582e73834587d409d0
Bader Idris <baidsforbusiness@gmail.com>
2022-12-08 16:08:11 +0200

    Aye

--- JavaScript-Course/Starting.js
+++ JavaScript-Course/Starting.js
@@ -1,3 +1,23 @@
-console.assert(username);
-var sth = username;
-var testing = console.log('Hello World', sth);
+/*
+  7 in 1 Course 188 lessons: JavaScript Bootcamp
+  1st JavaScript Core
+  2nd JavaScript OOP
+  3rd DOM
+  4th BOM
+  5th AJAX
+  6th JSON
+  7th ECMAScript
+*/
+// First lesson JS 2021
+/**
+ * Js is a scripting language
+ * We can use it with clint side, and server side by its frameworks
+ * It makes our websites interactive 
+ * Dynamic update content, using inputs and outputs
+ * Manipulating HTML and CSS content
+ * Animate Images and Creating Image Galleries
+ * Manipulating and validating Data
+ * Mobile & Desktop Apps, web Browser games
+ * Cross apps with specific frameworks [valid in IOS & windows] or any platform
+ * 
+ */
