77d0cb01acc50efb45eabd1773b77c06ad76f8c7
Bader Idris <baidsforbusiness@gmail.com>
2023-01-15 13:44:43 +0200

    beforeLeetCode 35th

---
+++ JavaScript-Course/Elzero-Assignments/4-from_023-026-assign.js
@@ -0,0 +1,27 @@
+// Examples
+console.log(100_000); // 100000
+console.log(100000); // 100000
+console.log(5e4 + 5e4); // 100000
+
+// Your Solutions
+console.log(10 ** 5); // 100000
+console.log(1e1 ** 5); // 100000
+console.log(5e1 ** 1); // 100000
+console.log(1e1 ** 5); // 100000
+console.log(100_000 * 1); // 100000
+console.log(200_000 / 2); // 100000
+console.log(100000 - 0); // 100000
+console.log(); // 100000
+console.log(); // 100000
+console.log(); // 100000
+// 1st assignment
+console.log(-Number.MIN_SAFE_INTEGER); // 9007199254740991
+// 2nd assignment ✅
+console.log(Number.MAX_SAFE_INTEGER); // 16 with max safe integer
+// 3rd assignment 
+
+let myVar = "100.56789 Views";
+myVar.split(" ", 1).Math.floor();
+console.log(Math.floor(99.9)); // 100
+console.log(); // 100.57
+// 4th assignment ✅
