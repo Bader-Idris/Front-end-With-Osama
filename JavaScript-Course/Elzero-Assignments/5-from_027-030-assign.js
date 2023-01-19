5929b98530969bcd2b6cd76f1c123e8a69a5197a
Bader Idris <baidsforbusiness@gmail.com>
2023-01-19 23:26:43 +0200

    27 to 30 assignments✅

--- JavaScript-Course/Elzero-Assignments/5-from_027-030-assign.js
+++ JavaScript-Course/Elzero-Assignments/5-from_027-030-assign.js
@@ -1,8 +1,20 @@
 let userName = "Elzero";
 console.log(userName[0].toLowerCase()); // e
 console.log(userName.split("",1).join("").toLowerCase()); // e
-// console.log(userName.firstIndex()); // e
-// console.log(userName.includes("e").toLowerCase()); // e
-console.log(); // e
-console.log(); // eee
+console.log(userName.slice(3, 4));// e
+console.log(userName.charAt(3)); // e
+console.log(userName.substring(3, 4)); // e
+console.log(userName.trim().charAt(3));// bonus by me💪
+console.log(userName.split("",1).join("").toLowerCase().repeat(3)); // eee
 // bring back firstIndex of that string
+// 1st Assignment✅
+
+let word = "Elzero";
+let letterZ = "z";
+let letterE = "e";
+let letterO = "O";
+
+console.log(word.includes("z")); // True
+console.log(word.startsWith("E")); // True
+console.log(word.endsWith("o")); // True
+// 2nd Assignment✅
