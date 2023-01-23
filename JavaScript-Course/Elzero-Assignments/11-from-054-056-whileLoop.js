1c265e07e28791c13b76eebecfe14115f7592efe
Bader Idris <baidsforbusiness@gmail.com>
2023-01-23 18:07:04 +0200

    whileLoopIsDone✅＼（〇_ｏ）／

--- JavaScript-Course/Elzero-Assignments/11-from-054-056-whileLoop.js
+++ JavaScript-Course/Elzero-Assignments/11-from-054-056-whileLoop.js
@@ -4,6 +4,16 @@ let friends = ["Ahmed", "Sayed", "Ali", 1, 2, "Mahmoud", "Amany"];
 let index = 0;
 let counter = 0;
 
+while (index < friends.length) {
+  index++;
+  if (index == friends.length) break;/* Must be here, not downwards */
+  if (typeof friends[index] === 'number'){
+    continue;
+  } else if (!friends[index].startsWith("A")){
+    counter ++;
+    console.log(`${counter} => ${friends[index]}`);
+  }
+}
 /*
   Output
 
---
+++ JavaScript-Course/LeetCode-Solutions/2-Medium/2. Add Two Numbers.js
@@ -0,0 +1,5 @@
+var addTwoNumbers = function(l1, l2) {
+  if (l1 > 0 && l2 > 0) {
+    return l1 + l2;
+  }
+};
