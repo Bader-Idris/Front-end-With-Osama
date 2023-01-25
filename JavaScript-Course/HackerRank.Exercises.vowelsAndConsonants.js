4bcdd29cfa88db31ee245143e873ad70df920760
Bader Idris <baidsforbusiness@gmail.com>
2023-01-25 23:36:53 +0200

    hackerRank.exercise

--- JavaScript-Course/HackerRank.Exercises.vowelsAndConsonants.js
+++ JavaScript-Course/HackerRank.Exercises.vowelsAndConsonants.js
@@ -39,3 +39,23 @@ function vowelsAndConsonants(s) {
 }
 vowelsAndConsonants(["a","e","i","o","u","s","c"])
 
+// -------------------------------- after some fixed in same day ************🔼
+
+function vowelsAndConsonants(s) {
+  let vowels = ["a","e","i","o","u"];
+  let consonants = [];
+  for (let i = 0; i < s.length; i++) {
+    if(s[i].includes(vowels[i], s[i])) console.log(`A vowel I am|: ${s[i]}`);
+else continue//☠️⚠️ Must fix else conditions, as if duplicated, or consonants⚠️☠️
+		}
+}
+
+vowelsAndConsonants(["a","e","i","o","u","s","c"])
+   } else {
+       s.push(vowels)
+       console.log(`Iam ${vowels[i]}not a vowel`)
+       if(s.length >= 15) break;
+   }
+  }
+  
+}
