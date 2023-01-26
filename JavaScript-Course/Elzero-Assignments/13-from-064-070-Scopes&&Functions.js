5b6c3045a95d15832d46e1853f4bc7fd355e2640
Bader Idris <baidsforbusiness@gmail.com>
2023-01-26 22:45:53 +0200

    Solving the hardest Function yet Osama entitled me

--- JavaScript-Course/Elzero-Assignments/13-from-064-070-Scopes&&Functions.js
+++ JavaScript-Course/Elzero-Assignments/13-from-064-070-Scopes&&Functions.js
@@ -1,41 +1,44 @@
 
-
 function getDetails(zName, zAge, zCountry) {
-  function namePattern(zName) {//will be 2 strings
-    // split the name and return the first string normally and 2nd's only first letter as capital
-    zName.split(" ") 
-    for (let i = 0; i < zName.length; i++) {
-      zName.split(" ")[i]
-      if (zName[i] == 1) console.log(zName[i])
+  function namePattern(zName) {
+    let nigga = zName.split(" ");
+    if (nigga.length == 2) {
+      return nigga[0] +" "+ nigga[1][0].toUpperCase() + "." ;
     }
     // Osama Mohamed => Osama M.
     // Ahmed ali => Ahmed A.
-  }
+  };//✅
   function ageWithMessage(zAge) {
-    // if 1st typed return 2nd one, or reverse 
-
+    let elAge = zAge.split(" ");
+    for (let i = 0; i < elAge.length; i++) {
+      if(!isNaN(elAge[i]) && elAge[i] != 0) {
+        return `Your Age Is ${elAge[i]}`;
+      } else continue
+    };
     // 38 Is My Age => Your Age Is 38
     // 32 Is The Age => Your Age Is 32
-  }
+  };//✅
   function countryTwoLetters(zCountry) {
-    // return first 2 letters as capital
-
+    return (zCountry[0] + zCountry[1]).toUpperCase();
     // Egypt => You Live In EG
     // Syria => You Live In SY
-  }
+  };
   function fullDetails() {
     // Write Your Code Here
-  }
+    return `Hello ${namePattern(zName)}, ${ageWithMessage(zAge)}, You Live In ${countryTwoLetters(zCountry)}`;
+  };//✅
   return fullDetails(); // Do Not Edit This
-}
-
+};
 console.log(getDetails("Osama Mohamed", "38 Is My Age", "Egypt"));
 // Hello Osama M., Your Age Is 38, You Live In EG
 
 console.log(getDetails("Ahmed ali", "32 Is The Age", "Syria"));
 // Hello Ahmed A., Your Age Is 32, You Live In SY
 
-// 1st Assignment 💤✅
+// 1st Assignment ✅ toke 3 days to fix all of its bugs, although some functions need improvements,
+//  they're too basic and raw. I failed using for to pick two indexes, so I used a stupid method,
+//  picking each on its own. And That's A Disaster In Programming☠️🤬
+// ✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅
 
 // ⚠️🔴Turn both functions into arrow syntax🔴⚠️
 // 1st func🔽
