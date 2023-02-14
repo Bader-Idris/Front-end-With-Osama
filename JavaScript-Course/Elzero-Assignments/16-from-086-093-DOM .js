70798e0a7998f2f9fd9c26641b511783d306b690
Bader Idris <baidsforbusiness@gmail.com>
2023-02-14 09:14:16 +0200

    alive baby alive

--- JavaScript-Course/Elzero-Assignments/16-from-086-093-DOM .js
+++ JavaScript-Course/Elzero-Assignments/16-from-086-093-DOM .js
@@ -1,4 +1,16 @@
 
-// pick this HTML text by using 15 methods
-// ,4 as querySelector and 4 as querySelectorAll
+// pick this HTML text by using 15 methods,
+// 4 as querySelector and 4 as querySelectorAll
 <div id="elzero" class="element" name="js">JavaScript</div>
+
+let myIdElement = document.getElementById("elzero");
+let myTagElement = document.getElementsByTagName("div");
+let myClassElement = document.getElementsByClassName("element");
+let myQueryElement = document.querySelector(".special");
+let myAllQueryElements = document.querySelectorAll(".special");// if many
+
+let myQueryName = document.getElementsByName('js');
+console.log(myTagElement.getAttribute("class"));
+
+
+console.log(myTagElement[0]);
