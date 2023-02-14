abb6401b2e6a4836e802bf9a09ab16cc406d66b6
Bader Idris <baidsforbusiness@gmail.com>
2023-02-14 18:14:53 +0200

    H

--- JavaScript-Course/Elzero-Assignments/16-from-086-093-DOM .js
+++ JavaScript-Course/Elzero-Assignments/16-from-086-093-DOM .js
@@ -6,11 +6,27 @@
 let myIdElement = document.getElementById("elzero");
 let myTagElement = document.getElementsByTagName("div");
 let myClassElement = document.getElementsByClassName("element");
+let myQueryName = document.getElementsByName('js');
+// console.log(myTagElement.getAttribute("class"));
 let myQueryElement = document.querySelector(".special");
 let myAllQueryElements = document.querySelectorAll(".special");// if many
 
-let myQueryName = document.getElementsByName('js');
-console.log(myTagElement.getAttribute("class"));
+
+let myQuery2 = document.querySelector("#elzero");
+let myQuery3 = document.querySelector("js");
+let myQuery4 = document.querySelector(".special");
+let myQuerySeries = document.querySelector("body .special");//check before it
 
 
-console.log(myTagElement[0]);
+document.body.getAttribute()
+document.body.getAttributeNS()
+document.body.getAttributeNames()
+document.body.getAttributeNode()
+document.body.getAttributeNodeNS()
+
+// let myAllQuerySeries = document.querySelectorAll(".special");
+
+
+
+
+console.log(myTagElement[0]);//if it's applicable we can get a double of each, but let it as last choice
