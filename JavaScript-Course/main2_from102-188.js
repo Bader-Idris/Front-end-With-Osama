126dc88a222c43016054076f95ac11d7a39baa3a
Bader Idris <baidsforbusiness@gmail.com>
2023-03-16 09:39:07 +0200

    Get Date And Time

--- JavaScript-Course/main2_from102-188.js
+++ JavaScript-Course/main2_from102-188.js
@@ -1654,7 +1654,6 @@ Object.prototype.love = "Elzero Web School";// an example
 String.prototype.addDotBeforeAndAfter = function (val) {
   return `.${this}.`;
 };
-
 let misString = "Elzero";
 
 // 157th lesson 05:30:00 Object Meta Data And Descriptor Part 1
@@ -1670,10 +1669,10 @@ const myObject = {
   b: 2,
 };
 
-Object.defineProperty(myObject, "c", {
+Object.defineProperty(myObject, "c", {// every not typed property description is false⚠️
   writable: false,
-  enumerable: true,
-  configurable: false,
+  enumerable: true,//this is for looping and counting
+  configurable: false,// if false, you can't delete or modify the property
   value: 3,
 });
 
@@ -1686,10 +1685,110 @@ Object.defineProperty(myObject, "c", {
 
 myObject.c = 100;
 
-console.log(delete myObject.c);
+console.log(delete myObject.c);// he printed it for seeing boolean true
 
-for (let prop in myObject) {
+for (let prop in myObject) {// for in loop
   console.log(prop, myObject[prop]);
 }
+console.log(myObject);
+
+// 158th lesson 05:36:00 Object Meta Data And Descriptor Part 2
+/*
+  Object Meta Data And Descriptor
+  - Define Multiple Properties
+  - Check Descriptors
+*/
+
+const myObject = {
+  a: 1,
+  b: 2,
+};
+
+Object.defineProperties(myObject, {
+  c: {
+    configurable: true,
+    value: 3,
+  },
+  d: {
+    configurable: true,
+    value: 4,
+  },
+  e: {
+    configurable: true,
+    value: 5,
+  },
+});
 
 console.log(myObject);
+
+console.log(Object.getOwnPropertyDescriptor(myObject, "d"));
+console.log(Object.getOwnPropertyDescriptors(myObject));
+
+// 159th lesson 05:38:30 Date And Time Introduction
+/*
+  Date And Time
+  - Date Constructor
+
+  Static Methods
+  - Date.now()
+
+  - To Track Time You Need Starting Point
+  - Epoch Time Or Unix Time In Computer Science Is The Number of Seconds Since January 1, 1970.
+  - Why 1970 [829 Days To 136 Years]
+
+  Search For
+  - Year 2038 Problem in Computer Science.
+  for more advanced search for [unix time]
+*/
+let dateNow = new Date();
+
+console.log(dateNow);
+
+console.log(Date.now()); // 1000 Mill = 1 Second
+
+let seconds = Date.now() / 1000; // Number Of Seconds
+console.log(`Seconds ${seconds}`);
+
+let minutes = seconds / 60; // Number Of Minutes
+console.log(`Minutes ${minutes}`);
+
+let hours = minutes / 60; // Number Of Hours
+console.log(`Hours ${hours}`);
+
+let days = hours / 24; // Number Of Days
+console.log(`Days ${days}`);
+
+let years = days / 365; // Number Of Years
+console.log(`Years ${years}`);
+// see https://timeanddate.com it gives current time GoTo[time zones] => time zone converter
+// the most useful this here is that prior lessons are constructors of current one
+
+// 160th lesson 05:45:35 Get Date And Time
+/*
+  Date And Time
+  - getTime() => Number Of Milliseconds
+  - getDate() => Day Of The Month
+  - getFullYear()
+  - getMonth() => Zero Based
+  - getDay() => Day Of The Week
+  - getHours()
+  - getMinutes()
+  - getSeconds()
+*/
+
+let dateNow = new Date();
+let birthday = new Date("Oct 25, 82");
+let dateDiff = dateNow - birthday;
+
+console.log(dateDiff);
+console.log(dateDiff / 1000 / 60 / 60 / 24 / 365);
+
+console.log(dateNow);
+console.log(dateNow.getTime());
+console.log(dateNow.getDate());
+console.log(dateNow.getFullYear());
+console.log(dateNow.getMonth());
+console.log(dateNow.getDay());
+console.log(dateNow.getHours());
+console.log(dateNow.getMinutes());
+console.log(dateNow.getSeconds());
