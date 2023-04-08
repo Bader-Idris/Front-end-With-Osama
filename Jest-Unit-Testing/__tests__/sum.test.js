75eda97b264a1d78946dfb509a4bbf1a92b503e2
Bader Idris <baidsforbusiness@gmail.com>
2023-04-08 15:40:02 +0300

    jest fixes and preceding

--- Jest-Unit-Testing/__tests__/sum.test.js
+++ Jest-Unit-Testing/__tests__/sum.test.js
@@ -1,21 +1,30 @@
 const theSum = require("../sum")
 // test(name, fn, timeout) its syntax
 // it(name, fn, timeout) test alias
-test("Returns The Summed Numbers", () => {
-  expect(theSum()).toBe(0);
-});
 
-test("Returns The Number", () => {
-  expect(theSum(10)).toBe(10);
-});
+describe("checking Sum Of Numbers", () => {// 6th lesson Describe; concatenating all prior lessons inside it
+  // test(name, fn, timeout); this is called test block
+  // describe(name, fn); we can use it to align functions as its sections
+  // we can nest describe, as other methods
+  describe('checking if input is null or one only parameter', () => {
+    // this is a nested describe
+    test("Returns The Summed Numbers", () => {
+      expect(theSum()).toBe(0);
+    });
 
-test("Returns The Summed Numbers", () => {
-  expect(theSum(15, 25)).toBe(40);
-});
+    test("Returns The Number", () => {
+      expect(theSum(10)).toBe(10);
+    });
+  })
 
-// 5th lesson
-test("Returns The Summed Numbers with rest para", () => {
-  expect(theSum(1, 9, 10, 40, 20, 10, 100, 10)).toBe(200);
-});
+  test("Returns The Summed Numbers", () => {
+    expect(theSum(15, 25)).toBe(40);
+  });
 
-console.log([].reduce((a,b)=> a + b, 0));
+  // 5th lesson
+  test("Returns The Summed Numbers with rest para", () => {
+    expect(theSum(1, 9, 10, 40, 20, 10, 100, 10)).toBe(200);
+  });
+
+  console.log([].reduce((a,b)=> a + b, 0));
+});
