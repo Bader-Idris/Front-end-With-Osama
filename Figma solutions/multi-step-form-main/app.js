af121e0ef4c988cd446845b92dae61433ac27348
Bader Idris <baidsforbusiness@gmail.com>
2023-04-24 19:25:31 +0300

    some fixes

--- Figma solutions/multi-step-form-main/app.js
+++ Figma solutions/multi-step-form-main/app.js
@@ -1,3 +1,21 @@
-const $All = (s) => document.querySelectorAll(s),
-      $ = (s) => document.querySelector(s)
+const $$ = (s) => document.querySelectorAll(s),
+      $ = (s) => document.querySelector(s),
+      forms = $$('main form'),
+      section = $('main section'),
+      nextButton = $('form button.next'),
+      backButton = $('form button.back')
       ;
+
+nextButton.onclick = () => {
+  section.style.top = "-100%";// make it dynamic
+};
+backButton.onclick = () => {
+  section.style.top = "+100%";// make it dynamic
+};
+// "540px" top: -540px; form when 1st click
+// section.style.top = "-100%";
+// forms[0].style.top = "-100%";
+// forms[1].style.top = "-100%";
+
+// section
+// top: -100%;
