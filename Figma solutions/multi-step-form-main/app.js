8dd818ec96d61beb4e041bf82538f37588f9dc75
Bader Idris <baidsforbusiness@gmail.com>
2023-04-25 16:33:47 +0300

    arrived at 4th, before responsible phones

--- Figma solutions/multi-step-form-main/app.js
+++ Figma solutions/multi-step-form-main/app.js
@@ -2,20 +2,64 @@ const $$ = (s) => document.querySelectorAll(s),
       $ = (s) => document.querySelector(s),
       forms = $$('main form'),
       section = $('main section'),
-      nextButton = $('form button.next'),
-      backButton = $('form button.back')
+      nextButton = $$('form button.next'),
+      backButton = $$('form button.back'),
+      submitBtn = $('form button[type="submit"]'),
+      steps = $$('aside div span'),
+      miniPlans = $$('.mini-plans div'),
+      planSpan = $('.plan-option span:nth-of-type(2)'),
+      imgs = $$('img'),
+      checkContainer = $$('.third .check-container')
       ;
 
-nextButton.onclick = () => {
-  section.style.top = "-100%";// make it dynamic
-};
-backButton.onclick = () => {
-  section.style.top = "+100%";// make it dynamic
-};
-// "540px" top: -540px; form when 1st click
-// section.style.top = "-100%";
-// forms[0].style.top = "-100%";
-// forms[1].style.top = "-100%";
+section.style.top = '0%';
+imgs.forEach((e)=> e.setAttribute('draggable', 'false'));
 
-// section
-// top: -100%;
+nextButton.forEach((e, ind) => {
+  e.onclick = () => {
+    section.style.top = +section.style.top.slice(0, -1) - 100 + "%";
+    steps.forEach((ev, i, arr) => {
+      arr[ind].classList.remove('active');
+      if (arr[ind + 1] !== undefined) {
+        arr[ind + 1].classList.add('active');
+      } else {
+        arr[ind].classList.add('active');
+      }
+    });
+  };
+})
+backButton.forEach((e, ind)=> {
+  e.onclick = () => {
+    section.style.top = +section.style.top.slice(0,-1) + 100 + "%";
+    steps.forEach((ev, i, arr) => {
+      arr[ind + 1].classList.remove('active');
+        arr[ind].classList.add('active');
+    });
+  };
+})
+submitBtn.addEventListener('click', function(e) {
+  e.preventDefault();
+  // perform form validation and submission using JavaScript
+});
+miniPlans.forEach((e) => {
+  e.onclick = () => {
+    e.classList.toggle('active');
+  };
+})
+planSpan.onclick = (e) => {
+  e.target.classList.toggle('active');
+  e.target.parentElement.children[0].classList.toggle('active');
+  e.target.parentElement.children[2].classList.toggle('active');
+}
+// yearly plans 
+// miniPlans[0].children[2].innerHTML = '$90/yr';
+// miniPlans[1].children[2].innerHTML = '$120/yr';
+// miniPlans[2].children[2].innerHTML = '$150/yr';
+
+// add p.innerHTML = "2 months free" to each
+
+checkContainer.forEach(e => e.onclick = () => {
+  e.children[0].checked = !e.children[0].checked;
+  // a lot of work is here;
+}
+);
