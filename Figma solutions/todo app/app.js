1953d25ba184380ca6d9fd41f6ec74b82f8c3498
Bader Idris <baidsforbusiness@gmail.com>
2023-05-11 16:25:35 +0300

    todo app dark design is done

--- Figma solutions/todo app/app.js
+++ Figma solutions/todo app/app.js
@@ -6,13 +6,16 @@ Element.prototype.$ = function (selector) {
   const elements = this.querySelectorAll(selector);
   return elements.length === 1 ? elements[0] : elements;
 };
-const dayToggle = $('.theme div')
+const dayToggle = $('.theme div'),
+      backGrounds = $('.back-img div')
   ;
 
 [...dayToggle].forEach(el => {
   el.addEventListener('click',(e) => {
     e.stopPropagation();
-    console.log(e.target);
-  
+    document.body.classList.toggle('dark');
+    [...dayToggle][0].classList.toggle('active');
+    [...dayToggle][1].classList.toggle('active');
+    [...backGrounds].forEach(bg => bg.classList.toggle('active'));
   });
 });
