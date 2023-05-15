d17d8a3e6b21f8c91d51e795ec941e1e36a3cd84
Bader Idris <baidsforbusiness@gmail.com>
2023-05-15 14:55:34 +0300

    continuing Template 3 with some JS

--- Template_3/js/main.js
+++ Template_3/js/main.js
@@ -1,55 +1,28 @@
-/*
- ** Countdown Timer
- ** Video URL: https://www.youtube.com/watch?v=eFsiOTJrrE8
-*/
+const $ = (selector) => {
+  const elements = document.querySelectorAll(selector);
+  return elements.length === 1 ? elements[0] : elements;
+};
+Element.prototype.$ = function (selector) {
+  const elements = this.querySelectorAll(selector);
+  return elements.length === 1 ? elements[0] : elements;
+};
+const ParentMegaMenu = $('.mega-menu').parentElement,
+      progressSpans = $(".the-progress span"),
+      section = $(".our-skills"),
+      nums = $(".stats .number"),
+      statsSection = $(".stats")
+;
 
-// The End Of The Year Date
-// 1000 milliseconds = 1 Second
-
-let countDownDate = new Date("Dec 31, 2022 23:59:59").getTime();
-// console.log(countDownDate);
-
-let counter = setInterval(() => {
-  // Get Date Now
-  let dateNow = new Date().getTime();
-
-  // Find The Date Difference Between Now And Countdown Date
-  let dateDiff = countDownDate - dateNow;
-
-  // Get Time Units
-  // let days = Math.floor(dateDiff / 1000 / 60 / 60 / 24);
-  let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
-
-  document.querySelector(".days").innerHTML = days < 10 ? `0${days}` : days;
-  document.querySelector(".hours").innerHTML = hours < 10 ? `0${hours}` : hours;
-  document.querySelector(".minutes").innerHTML = minutes < 10 ? `0${minutes}` : minutes;
-  document.querySelector(".seconds").innerHTML = seconds < 10 ? `0${seconds}` : seconds;
-
-  if (dateDiff < 0) {
-    clearInterval(counter);
+ParentMegaMenu.onclick = (e) => {
+  ParentMegaMenu.$('.mega-menu').classList.toggle('open');
+};
+window.addEventListener('click', (e) => {
+  if (e.target != ParentMegaMenu.children[0]) {
+    ParentMegaMenu.$('.mega-menu').classList.remove('open');
   }
-}, 1000);
+});
 
-/*
- ** Animate Width On Scrolling
- ** Video URL: https://youtu.be/sbIoIKI9FOc
- */
-
-/*
- ** Increase Numbers On Scrolling
- ** Video URL: https://youtu.be/PLsUdgLnzgQ
- */
-
-let progressSpans = document.querySelectorAll(".the-progress span");
-let section = document.querySelector(".our-skills");
-
-let nums = document.querySelectorAll(".stats .number");
-let statsSection = document.querySelector(".stats");
 let started = false; // Function Started ? No
-
 window.onscroll = function () {
   // Skills Animate Width
   if (window.scrollY >= section.offsetTop - 250) {
@@ -65,7 +38,6 @@ window.onscroll = function () {
     started = true;
   }
 };
-
 function startCount(el) {
   let goal = el.dataset.goal;
   let count = setInterval(() => {
@@ -75,31 +47,46 @@ function startCount(el) {
     }
   }, 2000 / goal);
 }
+
 
 
 /*
- ** Increase Numbers On Scrolling
- ** Video URL: https://www.youtube.com/watch?v=PLsUdgLnzgQ
+ ** Animate Width On Scrolling
+ ** Video URL: https://youtu.be/sbIoIKI9FOc
+ */
+
+
+/*
+ ** Countdown Timer
+ ** Video URL: https://www.youtube.com/watch?v=eFsiOTJrrE8
 */
-let nums = document.querySelectorAll(".nums .num");
-let section = document.querySelector(".three");
-let started = false; // Function Started ? No
 
-window.onscroll = function () {
-  if (window.scrollY >= section.offsetTop) {
-    if (!started) {
-      nums.forEach((num) => startCount(num));
-    }
-    started = true;
-  }
-};
+// The End Of The Year Date
+// 1000 milliseconds = 1 Second
 
-function startCount(el) {
-  let goal = el.dataset.goal;
-  let count = setInterval(() => {
-    el.textContent++;
-    if (el.textContent == goal) {
-      clearInterval(count);
-    }
-  }, 2000 / goal);
-}
+// let countDownDate = new Date("Dec 31, 2023 23:59:59").getTime();
+// // console.log(countDownDate);
+
+// let counter = setInterval(() => {
+//   // Get Date Now
+//   let dateNow = new Date().getTime();
+
+//   // Find The Date Difference Between Now And Countdown Date
+//   let dateDiff = countDownDate - dateNow;
+
+//   // Get Time Units
+//   // let days = Math.floor(dateDiff / 1000 / 60 / 60 / 24);
+//   let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+//   let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+//   let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
+
+//   document.querySelector(".days").innerHTML = days < 10 ? `0${days}` : days;
+//   document.querySelector(".hours").innerHTML = hours < 10 ? `0${hours}` : hours;
+//   document.querySelector(".minutes").innerHTML = minutes < 10 ? `0${minutes}` : minutes;
+//   document.querySelector(".seconds").innerHTML = seconds < 10 ? `0${seconds}` : seconds;
+
+//   if (dateDiff < 0) {
+//     clearInterval(counter);
+//   }
+// }, 1000);
