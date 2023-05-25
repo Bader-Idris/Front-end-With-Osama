8c611b963067985db14ccf773731aae44e3b3f0b
Bader Idris <baidsforbusiness@gmail.com>
2023-05-25 15:52:15 +0300

    moved modules out of final, to test with John

--- Node.js/with-John-Smilga/02-express-tutorial/03-express-basics.js
+++ Node.js/with-John-Smilga/02-express-tutorial/03-express-basics.js
@@ -7,14 +7,14 @@ const app = express()
 
 app.get('/', (req, res) => {
   console.log('user hit the resource')
-  res.status(200).send('Home Page')
+  res.status(200).send('Home Page')//! it's much better adding status code before sending
 })
 
 app.get('/about', (req, res) => {
   res.status(200).send('About Page')
 })
 
-app.all('*', (req, res) => {
+app.all('*', (req, res) => {// this calls all methods post, get etc
   res.status(404).send('<h1>resource not found</h1>')
 })
 
--- Node.js/with-John-Smilga/02-express-tutorial/04-express-app.js
+++ Node.js/with-John-Smilga/02-express-tutorial/04-express-app.js
@@ -4,10 +4,13 @@ const path = require('path')
 const app = express()
 
 // setup static and middleware
-app.use(express.static('./public'))
+app.use(express.static('./public'))//! important
+// this app.use is on behalf of all http 2 module,
+//  when we called all separated modules, on their own
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+  //! join does same here instead of resolve
 })
 
 app.all('*', (req, res) => {
