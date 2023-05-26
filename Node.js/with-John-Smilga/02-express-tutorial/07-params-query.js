9c80949f262d165fecb44e8e4f58260ee39db7e6
Bader Idris <baidsforbusiness@gmail.com>
2023-05-27 00:32:20 +0300

    more node && express

--- Node.js/with-John-Smilga/02-express-tutorial/07-params-query.js
+++ Node.js/with-John-Smilga/02-express-tutorial/07-params-query.js
@@ -15,42 +15,49 @@ app.get('/api/products', (req, res) => {
 
   res.json(newProducts)
 })
+//this : is called rout parameter, and productID is a placeholder
+// and this approach is better than requesting each id manually
 app.get('/api/products/:productID', (req, res) => {
   // console.log(req)
   // console.log(req.params)
   const { productID } = req.params
 
   const singleProduct = products.find(
-    (product) => product.id === Number(productID)
+    (product) => product.id === Number(productID)// here we get any id number
+    // if string we don't need to include inside Number method
   )
-  if (!singleProduct) {
+  if (!singleProduct) {//meaning if not existing, or undefined
     return res.status(404).send('Product Does Not Exist')
   }
 
   return res.json(singleProduct)
 })
-
+//! this is to show that rout can be more complex
 app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
   console.log(req.params)
   res.send('hello world')
 })
 
 app.get('/api/v1/query', (req, res) => {
-  // console.log(req.query)
-  const { search, limit } = req.query
+  // console.log(req.query)// this is how to see url-bar value provided
+  const { search, limit } = req.query// these are if user has 2 specific vars
+  // it responses to server, as search, limit object keys
   let sortedProducts = [...products]
 
-  if (search) {
+  if (search) {// if we make it search=a it returns only obj has names starting with a
     sortedProducts = sortedProducts.filter((product) => {
       return product.name.startsWith(search)
+      //? always use return inside responding conditions
     })
   }
-  if (limit) {
+  if (limit) {// this is for the length of array from products
     sortedProducts = sortedProducts.slice(0, Number(limit))
   }
   if (sortedProducts.length < 1) {
     // res.status(200).send('no products matched your search');
-    return res.status(200).json({ sucess: true, data: [] })
+    return res.status(200).json({ success: true, data: [] })// more common than commented
+    // if we don't return it, server will throw an error
+    //! we cannot send two responses in the same request
   }
   res.status(200).json(sortedProducts)
 })
@@ -58,3 +65,4 @@ app.get('/api/v1/query', (req, res) => {
 app.listen(5000, () => {
   console.log('Server is listening on port 5000....')
 })
+// check this: hacker news algolia api
--- Node.js/with-John-Smilga/02-express-tutorial/08-middleware-basic.js
+++ Node.js/with-John-Smilga/02-express-tutorial/08-middleware-basic.js
@@ -1,17 +1,29 @@
+//! we can literally say: middleware is everywhere in Express
+// express apps are nothing but punch of middleware functions stuffed together, to make one nice express cake/dessert🎂
 const express = require('express')
 const app = express()
 
 //  req => middleware => res
 
-const logger = (req, res, next) => {
-  const method = req.method
-  const url = req.url
-  const time = new Date().getFullYear()
-  console.log(method, url, time)
-  next()
-}
+// const logger = (req, res, next) => {//we moved it as an entire module
+//   //! it's better to have this function in a separate file
+//   const method = req.method
+//   const url = req.url
+//   const time = new Date().getFullYear()
+//   console.log(method, url, time)
+//   next()
+// }
+const logger = require('./logger')
+// app.use(logger) //this makes our logger function works with all paths
+//? order matters, if below / [main path] it won't be used
+// app.use('/api',logger)//! this makes all under api works with this func
+const authorize = require('./authorize')
+// app.use([logger, authorize])//! this is how to use many functions as middleware, i think it's destructuring
+//? order matters
 
-app.get('/', logger, (req, res) => {
+app.get('/', logger, (req, res) => {//! we put the function in the middle
+  // express supplies parameters inside the function above
+  //! we must pass funcs to a next middleware [⚠️IMPORTANT⚠️], If we don't send res manually
   res.send('Home')
 })
 app.get('/about', logger, (req, res) => {
@@ -21,3 +33,4 @@ app.get('/about', logger, (req, res) => {
 app.listen(5000, () => {
   console.log('Server is listening on port 5000....')
 })
+// https://expressjs.com/en/4x/api.html#app.use //! to learn more about app.use()
