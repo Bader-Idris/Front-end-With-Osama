d7ab77b262bda4fe3004adb5e9bd3a938033e6b0
Bader Idris <baidsforbusiness@gmail.com>
2023-06-03 18:33:47 +0300

    uploading 3, 4, 5, 6 project node&expressJohn👨‍🏫

---
+++ Node.js/with-John-Smilga/03-task-manager/app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+// middleware
+
+app.use(express.static('./public'));
+app.use(express.json());
+
+// routes
+
+app.use('/api/v1/tasks', tasks);
+
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
+
+
+//! at the introduction of 1st project John spoke about the importance of
+//  persisting data and performing all CRUD operations which stands for
+//? create, read, update, and delete
+//  which is fundamental to any application
