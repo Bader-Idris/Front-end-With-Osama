61c5cb889597e4316522ce257993fb98591ea832
Bader Idris <baidsforbusiness@gmail.com>
2023-06-08 19:52:49 +0300

    before losing my working on svg

--- JavaScript-Course/Not-with-Osama/chessGameWithAniaKubow/chessGameWithAniaKubow/javascript.js
+++ JavaScript-Course/Not-with-Osama/chessGameWithAniaKubow/chessGameWithAniaKubow/javascript.js
@@ -1,43 +1,28 @@
+// import * from './pieces';
 const gameBoard = document.querySelector('#gameboard'),
       playerDisplay = document.querySelector('#player'),
       infoDisplay = document.querySelector('#info-display'),
       width = 8
 ;
-let startPieces = []
-const whitePieces = ['bishop-black.svg','king-black.svg','knight-black.svg','pawn-black.svg','queen-black.svg','rook-black.svg']
-const blackPieces = ['bishop-white.svg','king-white.svg','knight-white.svg','pawn-white.svg','queen-white.svg','rook-white.svg']
-const pathWhite = [...whitePieces].forEach((e)=> {
-  addingImgs(e, gameBoard)
-})
-const pathBlack = [...blackPieces].forEach((e)=> {
-  addingImgs(e, gameBoard)
-})
-function addingImgs(e, dom) {
-  let img = document.createElement("img"),
-      div = document.createElement("div")
-  img.src = `./images/${e}`
-  img.classList.add(e.slice(0,-4))
-  img.style.width = 30 + "px"
-  div.className = 'piece'
-  div.id = e.slice(0,-4);
-  div.appendChild(img)
-  startPieces.push(div)
-}
-for (let i = 0; i < 8; i++) {
-  startPieces.push(startPieces[3].cloneNode(true),startPieces[9].cloneNode(true),'','','','')
-}
-startPieces.sort().reverse()
-
-for (let i = 0; i < startPieces.length - (8 * 4); i++) {
-  document.body.appendChild(startPieces[i])
-}
-for (let i = 0; i < 2; i++) {
-  startPieces.push(
-    startPieces[16].cloneNode(true),
-    startPieces[19].cloneNode(true),
-    startPieces[21].cloneNode(true),
-    startPieces[22].cloneNode(true),
-    startPieces[25].cloneNode(true),
-    startPieces[27].cloneNode(true)
-  )
-}
+let startPieces = [
+  rookDivBlack, knightDivBlack, bishopDivBlack, kingDivBlack, queenDivBlack, bishopDivBlack, knightDivBlack, rookDivBlack, 
+  pawnDivBlack, pawnDivBlack, pawnDivBlack, pawnDivBlack, pawnDivBlack, pawnDivBlack, pawnDivBlack, pawnDivBlack,
+  //! now put empty pieces
+  '','','','','','','','',
+  '','','','','','','','',
+  '','','','','','','','',
+  '','','','','','','','',
+  //! -----------
+  pawnDivWhite, pawnDivWhite, pawnDivWhite, pawnDivWhite, pawnDivWhite, pawnDivWhite, pawnDivWhite, pawnDivWhite, 
+  rookDivWhite, knightDivWhite, bishopDivWhite, kingDivWhite, queenDivWhite, bishopDivWhite, knightDivWhite, rookDivWhite, 
+]
+function createBoard () {
+  startPieces.forEach((sPiece, i)=> {
+    const square = document.createElement('div')
+    square.setAttribute('square-id', i)
+    square.append(sPiece)
+    square.className = 'square'
+    gameBoard.append(square)
+  })
+};
+createBoard()
--- JavaScript-Course/Not-with-Osama/chessGameWithAniaKubow/chessGameWithAniaKubow/pieces.js
+++ JavaScript-Course/Not-with-Osama/chessGameWithAniaKubow/chessGameWithAniaKubow/pieces.js
@@ -1,1 +1,22 @@
-console.log("Hello World!")
+const bishopDivBlack = createAnElement('div','','piece bishop black','','',"<img src='./images/bishop-black.svg' />"),
+      bishopDivWhite = createAnElement('div','','piece bishop white','','',"<img src='./images/bishop-white.svg' />"),
+      kingDivBlack = createAnElement('div','','piece king black','','',"<img src='./images/king-black.svg' />"),
+      kingDivWhite = createAnElement('div','','piece king white','','',"<img src='./images/king-white.svg' />"),
+      knightDivBlack = createAnElement('div','','piece king black','','',"<img src='./images/knight-black.svg' />"),
+      knightDivWhite = createAnElement('div','','piece king white','','',"<img src='./images/knight-white.svg' />"),
+      queenDivBlack = createAnElement('div','','piece queen black','','',"<img src='./images/queen-black.svg' />"),
+      queenDivWhite = createAnElement('div','','piece queen white','','',"<img src='./images/queen-white.svg' />"),
+      rookDivBlack = createAnElement('div','','piece rook black','','',"<img src='./images/rook-black.svg' />"),
+      rookDivWhite = createAnElement('div','','piece rook white','','',"<img src='./images/rook-white.svg' />"),
+      pawnDivBlack = createAnElement('div','','piece pawn black','','',"<img src='./images/pawn-black.svg' />"),
+      pawnDivWhite = createAnElement('div','','piece pawn white','','',"<img src='./images/pawn-white.svg' />")
+;
+function createAnElement(elementType,elementText, elementClass = '', elId = '', elSrc = '',elInner = '') {
+  let el = document.createElement(elementType)
+  el.textContent = elementText
+  el.classList = elementClass
+  el.id = elId
+  el.src = elSrc
+  el.innerHTML = elInner
+  return el
+}
