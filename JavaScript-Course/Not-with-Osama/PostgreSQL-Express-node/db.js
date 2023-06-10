aeb6e7b9ea9810df4267f71828e417c6ced0b4a9
Bader Idris <baidsforbusiness@gmail.com>
2023-06-11 00:56:22 +0300

    found the bug, when starting psql, see its port!

--- JavaScript-Course/Not-with-Osama/PostgreSQL-Express-node/db.js
+++ JavaScript-Course/Not-with-Osama/PostgreSQL-Express-node/db.js
@@ -5,6 +5,6 @@ const pool = new Pool({
   host: "localhost",
   database: "students",//our database we created in pg
   password: "admin",// the passwd I set, and saved in sticky
-  port: "5432",
+  port: "5433",// default is 5432 //! this MF wasted 2 hours of my life
 });
 module.exports = pool;
