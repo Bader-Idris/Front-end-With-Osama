1ced8efd47def6dde189cd92b8b6e8d62edbadd5
Bader Idris <baidsforbusiness@gmail.com>
2023-07-18 07:27:37 +0300

    DBs are ugly important big pile of 💩 sometimes

--- Figma solutions/Interactive-comments-section/server/db/comments_query.sql
+++ Figma solutions/Interactive-comments-section/server/db/comments_query.sql
@@ -1,7 +1,23 @@
 CREATE DATABASE comments;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 -- SELECT uuid_generate_v4();
+CREATE TABLE users(
+  user_uid UUID NOT NULL PRIMARY KEY,
+  f_name VARCHAR(50) NOT NULL,
+  l_name VARCHAR(50) NOT NULL,
+  email VARCHAR(255),
+  password VARCHAR(300) NOT NULL,
+  gender VARCHAR(6) NOT NULL,
+  dob DATE NOT NULL
+  );
 
+CREATE TABLE "user" ( user_uid UUID NOT NULL PRIMARY KEY,
+f_name VARCHAR(50) NOT NULL,
+l_name VARCHAR(50) NOT NULL,
+email VARCHAR(255),
+password VARCHAR(300) NOT NULL,
+gender VARCHAR(6) NOT NULL,
+dob DATE NOT NULL );
 ```
 for Events, TIMESTAMP is precision, it stores date data
 and time information
--- Figma solutions/Interactive-comments-section/server/models/user.js
+++ Figma solutions/Interactive-comments-section/server/models/user.js
@@ -2,8 +2,11 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const pool = require('../db/db');
 
+
+
 function createUser() {
   return `
+  CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
   CREATE TABLE user(
     user_uid UUID NOT NULL PRIMARY KEY,
     f_name VARCHAR(50) NOT NULL,
@@ -11,8 +14,7 @@ function createUser() {
     email VARCHAR(255),
     password VARCHAR(300) NOT NULL
     gender VARCHAR(6) NOT NULL,
-    dob DATE NOT NULL,
-  )
+    dob DATE NOT NULL);
   `
 }
 /*
@@ -22,23 +24,31 @@ function createUser() {
   make comments for each article =>
   comments = [{id, content, createdAt, score, user, replies}, more of same]
 */
-// create table car(
-//   car_uid UUID NOT NULL PRIMARY KEY,
-//   make VARCHAR(100) NOT NULL,
-//   model VARCHAR(100) NOT NULL,
-//   price NUMERIC(19, 2) NOT NULL CHECK(price > 0)
+// sql
+// CREATE TABLE users(
+//   user_id SERIAL PRIMARY KEY,
+//   username VARCHAR(255) NOT NULL,
+//   email VARCHAR(255) NOT NULL,
+//   --other user - related columns
 // );
-// create table person(
-//   person_uid UUID NOT NULL PRIMARY KEY,
-//   first_name VARCHAR(50) NOT NULL,
-//   last_name VARCHAR(50) NOT NULL,
-//   email VARCHAR(255),
-//   gender VARCHAR(6) NOT NULL,
-//   date_of_birth DATE NOT NULL,
-//   country_of_birth VARCHAR(50) NOT NULL,
-//   car_uid UUID REFERENCES car(car_uid),
-//   UNIQUE(car_uid),
-//   UNIQUE(email)
+//  CREATE TABLE comments(
+//   comment_id SERIAL PRIMARY KEY,
+//   post_id INT NOT NULL,
+//   user_id INT NOT NULL,
+//   content TEXT NOT NULL,
+//   created_at TIMESTAMP DEFAULT NOW(),
+//   FOREIGN KEY(post_id) REFERENCES posts(id),
+//   FOREIGN KEY(user_id) REFERENCES users(user_id)
+// );
+//  CREATE TABLE categories(
+//   category_id SERIAL PRIMARY KEY,
+//   category_name VARCHAR(255) NOT NULL
+// );
+//  CREATE TABLE postcategories(
+//   post_id INT NOT NULL,
+//   category_id INT NOT NULL,
+//   FOREIGN KEY(post_id) REFERENCES posts(id),
+//   FOREIGN KEY(category_id) REFERENCES categories(category_id)
 // );
 
 module.exports = pool;
